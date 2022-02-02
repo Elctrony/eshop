d865cb163f5b48e33c872f0a7dfe5cf552ea36fb
Elctrony <18064@stemegypt.edu.eg>
2022-02-02 15:25:09 +0200

    start off

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RootApp());
+}
+
+class RootApp extends StatelessWidget {
+  const RootApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
