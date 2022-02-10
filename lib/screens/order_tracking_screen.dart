2d995f2a4b8e11a7fc22c20f7b3ef543820d2db4
Elctrony <18064@stemegypt.edu.eg>
2022-02-10 19:30:58 +0200

    Order tracking shippinf

--- lib/screens/order_tracking_screen.dart
+++ lib/screens/order_tracking_screen.dart
@@ -52,7 +52,11 @@ class OrderTrackingScreen extends StatelessWidget {
                 itemCount: _items.length,
                 scrollDirection: Axis.horizontal,
                 itemBuilder: ((context, index) => TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      pageController.animateToPage(index,
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.ease);
+                    },
                     child: Text(
                       _items[index],
                       style: TextStyle(
@@ -69,6 +73,7 @@ class OrderTrackingScreen extends StatelessWidget {
                 controller: pageController,
                 children: [
                   PendingList(),
+                  ShippingList(),
                 ],
               ),
             ),
@@ -230,6 +235,129 @@ class PendingList extends StatelessWidget {
               )
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class ShippingList extends StatelessWidget {
+  ShippingList({Key? key}) : super(key: key);
+  final List<Map<String, dynamic>> listItem = [
+    {
+      'title': 'Apple Watch',
+      'img': 'assets/images/apple.png',
+      'description': 'Lorem Ipsum is simply dummy text of the printing and',
+      'price': 59.99,
+    },
+    {
+      'title': 'IPhone',
+      'img': 'assets/images/iphone.png',
+      'description': 'Lorem Ipsum is simply dummy text of the printing and',
+      'price': 1059.99,
+    },
+    {
+      'title': 'Air jorden',
+      'img': 'assets/images/airjorden.png',
+      'description': 'Lorem Ipsum is simply dummy text of the printing and',
+      'price': 79.99,
+    },
+    {
+      'title': 'Airpods',
+      'img': 'assets/images/airpod_product.png',
+      'description': 'Lorem Ipsum is simply dummy text of the printing and',
+      'price': 64.99,
+    },
+    {
+      'title': 'Amazon Speakre',
+      'img': 'assets/images/amazon.png',
+      'description': 'Lorem Ipsum is simply dummy text of the printing and',
+      'price': 159.99,
+    },
+    {
+      'title': 'Google shirt',
+      'img': 'assets/images/tshirt.png',
+      'description': 'Lorem Ipsum is simply dummy text of the printing and',
+      'price': 29.99,
+    },
+    {
+      'title': 'Speaker',
+      'img': 'assets/images/product3.png',
+      'description': 'Lorem Ipsum is simply dummy text of the printing and',
+      'price': 39.99,
+    }
+  ];
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return ListView.builder(
+      itemCount: listItem.length,
+      itemBuilder: (context, index) => Container(
+        height: 100,
+        margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 12),
+        padding: EdgeInsets.zero,
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(25)),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  bottomLeft: Radius.circular(25)),
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 12),
+                height: 100,
+                color: colorAvaliable[Random().nextInt(colorAvaliable.length)],
+                child: Image.asset(
+                  listItem[index]['img'] as String,
+                  width: size.width / 4,
+                  fit: BoxFit.scaleDown,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 12,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(0.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      listItem[index]['title'] as String,
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                          color: Color.fromRGBO(0, 0, 0, 1)),
+                    ),
+                    SizedBox(
+                      height: 40,
+                      child: Text(
+                        listItem[index]['description'] as String,
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w200,
+                            color: Colors.grey),
+                        overflow: TextOverflow.fade,
+                      ),
+                    ),
+                    Text('\$${listItem[index]['price'] as double}',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w400,
+                            color: Color.fromRGBO(0, 0, 0, 1))),
+                  ],
+                ),
+              ),
+            ),
+            CircleAvatar(
+                backgroundColor: primaryColor[900],
+                radius: 20,
+                child: SvgPicture.asset('assets/images/shipping.svg'))
+          ],
         ),
       ),
     );
